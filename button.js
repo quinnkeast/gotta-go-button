35a20a530b7ffff01e2622dee9b2f7f8bae14432
Quinn Keast <qkeast@sourcegraph.com>
2020-12-03 13:32:11 +0100

    Hacking

--- button.js
+++ button.js
@@ -1,49 +1,40 @@
 class Button {
-  constructor(activeIntervalSecs, activePeriodSecs, ledGpio, pushGpio) {
+  constructor(ledGpio, pushGpio) {
     this.ledGpio = ledGpio;
     this.pushGpio = pushGpio;
     this.activeIntervalMs = activeIntervalSecs * 1000;
     this.activePeriodSecs = activePeriodSecs;
     this.active = false;
-    this.blinkInterval;
-    //this.setTimer();
     
     this.pushGpio.watch(function (err, state) {
       if (err) throw err;
       
-      if (!this.active) return;
+      if (this.active) {
+        return;
+      }
       
-      this.resetTimer();
+      console.log('Big ass button pressed!!');
+      this.startBlink();
+      
     }.bind(this));
   }
   
-  setTimer() {
-    setTimeout(function() {
-      this.startBlink();
-    }.bind(this), this.activeIntervalMs);
-  }
-  
-  resetTimer() {
-    clearInterval(this.blinkInterval);
+  reset() {
     this.active = false;
     console.log('Big ass button is done now.');
     this.ledGpio.writeSync(0);
-    this.setTimer();
   }
   
   startBlink() {
     this.active = true;
-    console.log('Big ass button was activated!');
+    console.log('Big ass button gonna blink now');
     let count = 0;
-    this.blinkInterval = setInterval(function() {
-      if (count <= this.activePeriodSecs) {
-        this.ledGpio.writeSync(count % 2);
-        count++;
-      }
-      else {
-        this.resetTimer();
-      }
-    }.bind(this), 1000);
+    while (count <= 5) {
+      this.ledGpio.writeSync(count % 2);
+      count++;
+    } else {
+      this.reset();
+    }
   }
 }
 
