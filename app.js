39e63b2f2cf33ec8974288931a61c3e9ac0ae277
Quinn Keast <qkeast@sourcegraph.com>
2020-12-03 13:37:57 +0100

    Hacking

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 const dotenv = require("dotenv");
 const GPIO = require("onoff").Gpio;
-const Button = require("./button.js");
 
 dotenv.config();
 
 const buttonPushGpio = new GPIO(3, "in", "falling", {debounceTimeout: 10});
 const buttonLedGpio = new GPIO(4, "out");
 
-const button = new Button(5, 5, buttonLedGpio, buttonPushGpio);
+let active = false;
+
+function startBlink() {
+  active = true;
+  console.log('Big ass button gonna blink now');
+  
+  let count = 0;
+  
+  while (count <= 5) {
+    buttonLedGpio.writeSync(count % 2);
+    count++;
+  } 
+  
+  this.reset();
+}
+
+function reset() {
+  let active = false;
+}
 
 function exit() {
   buttonLedGpio.unexport();
   buttonPushGpio.unexport();
   process.exit();
 }
+
+buttonPushGpio.watch((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log('Big ass button pressed!!');
+  startBlink();
+});
 
 console.log("Big ass button is on!");
 
